5135e99d7a4798cca2c2504a2fd0ce7ef9f22588
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-27 09:55:59 +0300

    (refactor) router config

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'auth',
+    redirectTo: 'places',
     pathMatch: 'full'
   },
   {
@@ -14,30 +14,6 @@ const routes: Routes = [
   {
     path: 'places',
     loadChildren: () => import('./pages/places/places.module').then( m => m.PlacesPageModule)
-  },
-  {
-    path: 'discover',
-    loadChildren: () => import('./pages/discover/discover.module').then( m => m.DiscoverPageModule)
-  },
-  {
-    path: 'offers',
-    loadChildren: () => import('./pages/offers/offers.module').then( m => m.OffersPageModule)
-  },
-  {
-    path: 'new-offer',
-    loadChildren: () => import('./pages/new-offer/new-offer.module').then( m => m.NewOfferPageModule)
-  },
-  {
-    path: 'edit-offer',
-    loadChildren: () => import('./pages/edit-offer/edit-offer.module').then( m => m.EditOfferPageModule)
-  },
-  {
-    path: 'place-detail',
-    loadChildren: () => import('./pages/place-detail/place-detail.module').then( m => m.PlaceDetailPageModule)
-  },
-  {
-    path: 'offer-booking',
-    loadChildren: () => import('./pages/offer-booking/offer-booking.module').then( m => m.OfferBookingPageModule)
   },
   {
     path: 'bookings',
