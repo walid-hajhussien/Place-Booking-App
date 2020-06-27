2b55e6bba2c7a1fca195e1e6d1301561e582243d
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-27 13:25:01 +0300

    add places service

---
+++ src/app/models/placeModel/place.model.ts
@@ -0,0 +1,10 @@
+export class PlaceModel {
+    constructor(
+        public id: string,
+        public title: string,
+        public description: string,
+        public imageUrl: string,
+        public price: number
+    ) {
+    }
+}
