eb5bc10910cbdd0940f53a821947501b1e33eae3
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-27 11:13:52 +0300

    (add) routing config

--- src/app/pages/places/places-routing.module.ts
+++ src/app/pages/places/places-routing.module.ts
@@ -1,17 +1,40 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 
-import { PlacesPage } from './places.page';
+import {PlacesPage} from './places.page';
+
 
 const routes: Routes = [
-  {
-    path: '',
-    component: PlacesPage
-  }
+    {
+        path: '',
+        component: PlacesPage,
+        children: [
+            {
+                path: 'discover', children: [
+                    {path: '', loadChildren: () => import('../discover/discover.module').then(m => m.DiscoverPageModule)},
+                    {path: ':placeId', loadChildren: () => import('../place-detail/place-detail.module').then(m => m.PlaceDetailPageModule)}
+                ]
+            },
+            {
+                path: 'offers',
+                children: [
+                    {path: '', loadChildren: () => import('../offers/offers.module').then(m => m.OffersPageModule)},
+                    {path: 'new-offer', loadChildren: () => import('../new-offer/new-offer.module').then(m => m.NewOfferPageModule)},
+                    {path: 'edit/:id', loadChildren: () => import('../edit-offer/edit-offer.module').then(m => m.EditOfferPageModule)},
+                    {
+                        path: ':placeId',
+                        loadChildren: () => import('../offer-booking/offer-booking.module').then(m => m.OfferBookingPageModule)
+                    }
+                ]
+            },
+            {path: '', redirectTo: '/places/discover', pathMatch: 'full'}
+        ]
+    }
 ];
 
 @NgModule({
-  imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule],
+    imports: [RouterModule.forChild(routes)],
+    exports: [RouterModule],
 })
-export class PlacesPageRoutingModule {}
+export class PlacesPageRoutingModule {
+}
