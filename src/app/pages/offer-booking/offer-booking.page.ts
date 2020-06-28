a77ffe40632fa67f74355a62f1700b01222e760b
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-28 10:45:15 +0300

    add offer booking

--- src/app/pages/offer-booking/offer-booking.page.ts
+++ src/app/pages/offer-booking/offer-booking.page.ts
@@ -1,15 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlaceModel} from '../../models/placeModel/place.model';
+import {ActivatedRoute} from '@angular/router';
+import {NavController} from '@ionic/angular';
+import {PlacesService} from '../../services/places/places.service';
 
 @Component({
-  selector: 'app-offer-booking',
-  templateUrl: './offer-booking.page.html',
-  styleUrls: ['./offer-booking.page.scss'],
+    selector: 'app-offer-booking',
+    templateUrl: './offer-booking.page.html',
+    styleUrls: ['./offer-booking.page.scss'],
 })
 export class OfferBookingPage implements OnInit {
+    place: PlaceModel;
+    selectedId: string;
 
-  constructor() { }
+    constructor(private activatedRoute: ActivatedRoute, private navController: NavController, private placesService: PlacesService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.activatedRoute.paramMap.subscribe((paramMap) => {
+            if (!paramMap.has('placeId')) {
+                this.navController.navigateBack('/places/offers');
+                return;
+            }
+            this.selectedId = paramMap.get('placeId');
+            this.place = this.placesService.getPlaceById(this.selectedId);
+            console.log(this.place);
+
+        });
+    }
 
 }
