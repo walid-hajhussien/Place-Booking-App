784a6b7d233fcf5b82242bfba50d455c898ebad5
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-06-28 08:34:57 +0300

    add discover logic

--- src/app/pages/discover/discover.page.ts
+++ src/app/pages/discover/discover.page.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PlacesService} from '../../services/places/places.service';
+import {PlaceModel} from '../../models/placeModel/place.model';
 
 @Component({
-  selector: 'app-discover',
-  templateUrl: './discover.page.html',
-  styleUrls: ['./discover.page.scss'],
+    selector: 'app-discover',
+    templateUrl: './discover.page.html',
+    styleUrls: ['./discover.page.scss'],
 })
 export class DiscoverPage implements OnInit {
+    public places: PlaceModel[];
+    public selected: PlaceModel;
 
-  constructor() { }
+    constructor(private placesService: PlacesService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.places = this.placesService.places;
+        this.selected = this.places[0];
+        console.log(this.places);
+    }
+
+    onSelect(place: PlaceModel): void {
+        this.selected = place;
+    }
 
 }
