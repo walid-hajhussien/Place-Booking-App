7776f9efdb3822e259744fd58f4725f0c0fcea52
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-01 11:44:52 +0300

    add sliding

--- src/app/pages/offers/offers.page.ts
+++ src/app/pages/offers/offers.page.ts
@@ -1,20 +1,28 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {PlaceModel} from '../../models/placeModel/place.model';
 import {PlacesService} from '../../services/places/places.service';
+import {Router} from '@angular/router';
+import {IonItemSliding, NavController} from '@ionic/angular';
 
 @Component({
-  selector: 'app-offers',
-  templateUrl: './offers.page.html',
-  styleUrls: ['./offers.page.scss'],
+    selector: 'app-offers',
+    templateUrl: './offers.page.html',
+    styleUrls: ['./offers.page.scss'],
 })
 export class OffersPage implements OnInit {
-  offerPlaces: PlaceModel[];
+    offerPlaces: PlaceModel[];
 
-  constructor(private placesService: PlacesService) {
-    this.offerPlaces = this.placesService.places;
-  }
+    constructor(private placesService: PlacesService, private router: Router, private navController: NavController) {
+        this.offerPlaces = this.placesService.places;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
+
+    onEdit(id: string, itemSliding: IonItemSliding) {
+        itemSliding.close();
+        // note: we use navController to apply the animation
+        this.navController.navigateForward(['/', 'places', 'offers', 'edit', id]);
+    }
 
 }
