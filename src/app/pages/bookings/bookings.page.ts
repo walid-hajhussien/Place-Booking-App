49e6b525265e32d2eee68721f65305c34e3a8524
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-01 12:34:48 +0300

    add booking slides

--- src/app/pages/bookings/bookings.page.ts
+++ src/app/pages/bookings/bookings.page.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {BookingService} from '../../services/booking/booking.service';
+import {BookingModel} from '../../models/bookingModel/booking.model';
+import {IonItemSliding} from '@ionic/angular';
 
 @Component({
-  selector: 'app-bookings',
-  templateUrl: './bookings.page.html',
-  styleUrls: ['./bookings.page.scss'],
+    selector: 'app-bookings',
+    templateUrl: './bookings.page.html',
+    styleUrls: ['./bookings.page.scss'],
 })
 export class BookingsPage implements OnInit {
+    public bookings: BookingModel[];
 
-  constructor() { }
+    constructor(private bookingService: BookingService) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        this.bookings = this.bookingService.bookings;
+    }
+
+    onDeleteBooking(id: string, itemSliding: IonItemSliding) {
+        itemSliding.close();
+    }
 
 }
