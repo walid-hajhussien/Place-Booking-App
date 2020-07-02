47737f2858f7e0e46e66e2db9160447408314659
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-02 12:59:39 +0300

    add overlay Spinner

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,6 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {Router} from '@angular/router';
+import {LoadingController} from '@ionic/angular';
 
 @Component({
     selector: 'app-auth',
@@ -8,16 +9,29 @@ import {Router} from '@angular/router';
     styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
+    public isLoading: boolean;
 
-    constructor(private authService: AuthService, private router: Router) {
+    constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {
+        this.isLoading = false;
     }
 
     ngOnInit() {
     }
 
     onLogin() {
-        this.authService.login();
-        this.router.navigate(['/', 'places']);
+        // note : keyboardClose to close the phone keyboard
+        this.loadingController.create({
+            message: 'Loading..',
+            keyboardClose: true,
+            spinner: 'lines'
+        }).then((loadingREl: HTMLIonLoadingElement) => {
+            loadingREl.present();
+            setTimeout(() => {
+                loadingREl.dismiss();
+                this.authService.login();
+                this.router.navigate(['/', 'places']);
+            }, 2000);
+        });
     }
 
 }
