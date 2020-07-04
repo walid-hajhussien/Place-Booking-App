f274949053d0283b70ad87bc9f795763961a037d
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-04 11:54:16 +0300

    (refactor) login page

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,46 +1,47 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {AuthService} from '../../services/auth/auth.service';
 import {Router} from '@angular/router';
 import {LoadingController} from '@ionic/angular';
 import {NgForm} from '@angular/forms';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.page.html',
-  styleUrls: ['./login.page.scss'],
+    selector: 'app-login',
+    templateUrl: './login.page.html',
+    styleUrls: ['./login.page.scss'],
 })
 export class LoginPage implements OnInit {
-  public isLoading: boolean;
-  public errorMessage: string[];
-  public isLoginMode: boolean;
+    public isLoading: boolean;
+    public errorMessage: string[];
+    public isLoginMode: boolean;
 
-  constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {
-    this.isLoading = false;
-    this.isLoginMode = true;
-  }
+    constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {
+        this.isLoading = false;
+        this.isLoginMode = true;
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-  onLogin(form: NgForm) {
-    console.log(form);
-    // note : keyboardClose to close the phone keyboard
-    this.loadingController.create({
-        message: 'Loading..',
-        keyboardClose: true,
-        spinner: 'lines'
-    }).then((loadingREl: HTMLIonLoadingElement) => {
-        loadingREl.present();
-        setTimeout(() => {
-            loadingREl.dismiss();
-            this.authService.login();
-            this.router.navigate(['/', 'places']);
-        }, 2000);
-    });
-  }
+    onLogin(loginForm: NgForm) {
+        console.log(loginForm);
+        // note : keyboardClose to close the phone keyboard
+        this.loadingController.create({
+            message: 'Loading..',
+            keyboardClose: true,
+            spinner: 'lines'
+        }).then((loadingREl: HTMLIonLoadingElement) => {
+            loadingREl.present();
+            setTimeout(() => {
+                loginForm.reset();
+                loadingREl.dismiss();
+                this.authService.login();
+                this.router.navigate(['/', 'places']);
+            }, 2000);
+        });
+    }
 
-  onSwitchAuthMode() {
-    this.isLoginMode = !this.isLoginMode;
-  }
+    onSwitchAuthMode() {
+        this.isLoginMode = !this.isLoginMode;
+    }
 
 }
