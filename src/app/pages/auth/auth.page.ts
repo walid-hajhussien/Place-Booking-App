89be863af8983dba2d2131129d1569e6d5f84096
walid-hajhussien <eng.waleed_com@yahoo.com>
2020-07-04 12:53:05 +0300

    (add) new offer

--- src/app/pages/auth/auth.page.ts
+++ src/app/pages/auth/auth.page.ts
@@ -1,8 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {AuthService} from '../../services/auth/auth.service';
-import {Router} from '@angular/router';
-import {LoadingController} from '@ionic/angular';
-import {NgForm} from '@angular/forms';
+
 
 @Component({
     selector: 'app-auth',
@@ -10,37 +7,13 @@ import {NgForm} from '@angular/forms';
     styleUrls: ['./auth.page.scss'],
 })
 export class AuthPage implements OnInit {
-    public isLoading: boolean;
-    public errorMessage: string[];
-    public isLoginMode: boolean;
 
-    constructor(private authService: AuthService, private router: Router, private loadingController: LoadingController) {
-        this.isLoading = false;
-        this.isLoginMode = true;
+
+    constructor() {
     }
 
     ngOnInit() {
     }
 
-    onLogin(form: NgForm) {
-        console.log(form);
-        // note : keyboardClose to close the phone keyboard
-        // this.loadingController.create({
-        //     message: 'Loading..',
-        //     keyboardClose: true,
-        //     spinner: 'lines'
-        // }).then((loadingREl: HTMLIonLoadingElement) => {
-        //     loadingREl.present();
-        //     setTimeout(() => {
-        //         loadingREl.dismiss();
-        //         this.authService.login();
-        //         this.router.navigate(['/', 'places']);
-        //     }, 2000);
-        // });
-    }
-
-    onSwitchAuthMode() {
-        this.isLoginMode = !this.isLoginMode;
-    }
 
 }
